753cab76b7ccdd36bb961f4bba73e231bb476195
omerk42 <omerkhanak42@gmail.com>
2023-07-17 12:37:09 +0300

    nav style

--- src/routes/navBar/navBar.component.jsx
+++ src/routes/navBar/navBar.component.jsx
@@ -1,16 +1,16 @@
 import { Link, Outlet } from "react-router-dom";
-
-
+import { ReactComponent as MonkeyLogo } from "../../assets/images/monkey-logo.svg";
+import "./navBar.style.scss"
 const NavBar = () => {
     return (
       <>      
       <div className="nav">
         <div className="nav-logo-cont">
         <Link className="nav-logo" to='/'>
-            logo
+            <MonkeyLogo className="logo"/>
         </Link>
         </div>
-        <div className="nav-link-cont">
+        <div className="nav-links-cont">
             <Link className="nav-link" to='/tst'>
                 tst
             </Link>    
